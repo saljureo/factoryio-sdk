73b1a848183474a2cd541c7caddf6665587b5d1a
Santiago <saljureo@gmail.com>
2021-11-24 20:22:52 -0500

    Working on adding all states of supervisory control

--- samples/Controllers/Scenes/SupervisoryControl.cs
+++ samples/Controllers/Scenes/SupervisoryControl.cs
@@ -9,21 +9,67 @@ namespace Controllers.Scenes
         //private readonly McStatus mc1Status;
         //private readonly EventsMc0 eventsMc0;
         //private readonly EventsMc1 eventsMc1;
-
+        int stateCode;
+        int previousStateCode;
+        SupervisorState supervisorState;
+        SupervisorState previousState;
+        public enum SupervisorState
+        {
+            OK_E_i1_i2,
+            OK_E_w1_i2,
+            OK_F_i1_i2
+        }
         public SupervisoryControl()//(McStatus mc0Status, McStatus mc1Status, EventsMc0 eventsMc0, EventsMc1 eventsMc1)
         {
             //this.mc0Status = mc0Status;
             //this.mc1Status = mc1Status;
             //this.eventsMc0 = eventsMc0;
             //this.eventsMc1 = eventsMc1;
-            
+            supervisorState = SupervisorState.OK_E_i1_i2;//Initial State
+            previousState = SupervisorState.OK_E_w1_i2;
         }
 
-        public bool On(McStatus mc0Status, McStatus mc1Status)
+        public bool On(McStatus mc1Status, McStatus mc2Status, BufferStatus bufferStatus, BreakdownM2 breakdownM2,
+            EventsMc1 eventsMc1, EventsMc2 eventsMc2)
         {
-            Console.WriteLine("This is printed from SupervisoryControl Class. mc0Status = " + mc0Status);
-            Console.WriteLine("This is printed from SupervisoryControl Class. mc1Status = " + mc1Status);
-            return (true);
+            if (mc1Status == McStatus.IDLE && mc2Status == McStatus.IDLE && 
+                bufferStatus == BufferStatus.EMPTY && breakdownM2 == BreakdownM2.OK)//stateCode = 0
+            {
+                supervisorState = SupervisorState.OK_E_i1_i2;
+                if (supervisorState != previousState)
+                {
+                    Console.WriteLine("State: " + supervisorState);
+                    previousState = supervisorState;
+                }
+                if (eventsMc1 == EventsMc1.s1)
+                {
+                    return (true);
+                }
+                else if (eventsMc1 == EventsMc1.r1)
+                {
+                    return (false);
+                }
+            }
+            else if (mc1Status == McStatus.WORKING && mc2Status == McStatus.IDLE &&
+                bufferStatus == BufferStatus.EMPTY && breakdownM2 == BreakdownM2.OK)//stateCode = 1
+            {
+                supervisorState = SupervisorState.OK_E_w1_i2;
+                if (supervisorState != previousState)
+                {
+                    Console.WriteLine("State: " + supervisorState);
+                    previousState = supervisorState;
+                }
+                if (eventsMc1 == EventsMc1.s1)
+                {
+                    Console.WriteLine("Event " + eventsMc1 + " approved.");
+                    return (true);
+                }
+                else if (eventsMc1 == EventsMc1.r1)
+                {
+                    return (false);
+                }
+            }
+            return (false);
         }
 
 
